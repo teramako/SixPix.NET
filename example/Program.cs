769ee3244d2cac12349c85d010c3eb80dcca0d53
teramako <teramako@gmail.com>
2024-10-13 11:20:53 +0900

    Update example code

--- example/Program.cs
+++ example/Program.cs
@@ -1,3 +1,4 @@
+using SixLabors.ImageSharp.Formats.Png;
 using SixPix;
 
 Console.WriteLine("Hello, SixPix!");
@@ -7,9 +8,40 @@ if (args.Length == 0 || !Path.Exists(args[0]))
     Environment.Exit(1);
 }
 
-using var fs = File.OpenRead(args[0]);
+var fileInfo = new FileInfo(args[0]);
+using var fs = fileInfo.OpenRead();
+switch (fileInfo.Extension)
+{
+    case ".png":
+    case ".jpg":
+    case ".gif":
+    case ".bmp":
+        {
+            var start = DateTime.Now;
+            // Encode: Image stream -> Sixel string (ReadOnlySpan<char>)
+            var sixelString = Sixel.Encode(fs);
+            var elaps = DateTime.Now - start;
+            Console.WriteLine($"Elaps {elaps.TotalMilliseconds} ms");
 
-var sixelString = Sixel.Encode(fs);
-Console.Out.WriteLine(sixelString);
+            // Output to stdout
+            Console.Out.WriteLine(sixelString);
 
-Environment.Exit(0);
+            Environment.Exit(0);
+        }
+        break;
+    default:
+        {
+            var start = DateTime.Now;
+            // Decode: Sixel stream -> Image<Rgb24>
+            using var image = Sixel.Decode(fs);
+            var elaps = DateTime.Now - start;
+            Console.WriteLine($"Elaps {elaps.TotalMilliseconds} ms");
+
+            // Save image to a file as png
+            using var wf = new FileStream("./test.png", FileMode.Create);
+            Console.WriteLine(wf.Name);
+            image.Save(wf, new PngEncoder());
+        }
+        Environment.Exit(0);
+        break;
+}
