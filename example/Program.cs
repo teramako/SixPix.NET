d75c723573c1ab7771bbd0ea6f0a6208bb2aaa9b
Nutzzz <enussbaum@hotmail.com>
2025-05-09 11:36:38 -0700

    Fix missing closing bracket

--- example/Program.cs
+++ example/Program.cs
@@ -375,7 +375,7 @@ static void PrintUsage()
 
         Console.WriteLine($"Sixel is supported! [Cell Size:{cellSize.Width}x{cellSize.Height}; " +
             $"Background:#{Sixel.BackgroundColor}; " +
-            $"Current Window:{windowPixelSize.Width}x{windowPixelSize.Height}px, {windowCharSize.Width}x{windowCharSize.Height}ch");
+            $"Current Window:{windowPixelSize.Width}x{windowPixelSize.Height}px, {windowCharSize.Width}x{windowCharSize.Height}ch]");
     }
     else
         Console.WriteLine("If you see colored bands above, your terminal supports Sixel.");
